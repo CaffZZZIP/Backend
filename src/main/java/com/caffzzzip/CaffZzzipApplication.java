870d125dc4f5fb88d556e4e21e342d0b575c3d42
shtfh <syj2003826@gmail.com>
2026-04-24 21:28:14 +0900

    initial project setup

---
+++ src/main/java/com/caffzzzip/CaffZzzipApplication.java
@@ -0,0 +1,13 @@
+package com.caffzzzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffZzzipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffZzzipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caffzzzip/CaffZzzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caffzzzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffZzzipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
