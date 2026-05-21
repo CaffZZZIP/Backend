81d033b797acd22ea44347b14ae15d78dedfdfc1
shtfh <syj2003826@gmail.com>
2026-05-21 16:55:56 +0900

    feat: : 프엔 리다이렉트 url 추가

--- src/main/java/com/caffzzzip/kakao/api/KakaoAuthController.java
+++ src/main/java/com/caffzzzip/kakao/api/KakaoAuthController.java
@@ -1,15 +1,18 @@
 package com.caffzzzip.kakao.api;
 
-import com.caffzzzip.common.error.SuccessCode;
-import com.caffzzzip.common.template.ApiResTemplate;
 import com.caffzzzip.kakao.api.dto.KakaoLoginResponse;
 import com.caffzzzip.kakao.application.KakaoAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,17 +20,29 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class KakaoAuthController {
 
     private final KakaoAuthService kakaoAuthService;
+
+    @Value("${frontend.redirect-uri}")
+    private String frontendRedirectUri;
+
     @Operation(
             summary = "카카오 로그인",
-            description = "인가 코드를 받아 카카오 로그인 후 JWT를 발급합니다."
+            description = "인가 코드를 받아 카카오 로그인 후 JWT를 발급하고 프론트엔드로 리다이렉트합니다."
     )
     @GetMapping("/api/auth/kakao/callback")
-    public ApiResTemplate<KakaoLoginResponse> kakaoCallback(@RequestParam String code) {
+    public RedirectView kakaoCallback(@RequestParam String code) {
         KakaoLoginResponse response = kakaoAuthService.login(code);
 
-        return ApiResTemplate.successResponse(
-                SuccessCode.LOGIN_SUCCESS,
-                response
-        );
+        String redirectUrl = frontendRedirectUri
+                + "?accessToken=" + encode(response.accessToken())
+                + "&refreshToken=" + encode(response.refreshToken())
+                + "&isFirstLogin=" + response.isFirstLogin()
+                + "&userId=" + response.userId()
+                + "&nickname=" + encode(response.nickname());
+
+        return new RedirectView(redirectUrl);
+    }
+
+    private String encode(String value) {
+        return URLEncoder.encode(value, StandardCharsets.UTF_8);
     }
 }
