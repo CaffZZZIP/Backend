013cbb9cdad650f9882ff8970a6057857dc92910
shtfh <syj2003826@gmail.com>
2026-05-22 10:14:25 +0900

    feature: 프론트 연동을 위한 CORS 설정 추가

--- src/main/java/com/caffzzzip/global/config/SecurityConfig.java
+++ src/main/java/com/caffzzzip/global/config/SecurityConfig.java
@@ -4,12 +4,17 @@ import com.caffzzzip.global.jwt.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +31,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
 
                 // CORS 설정
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
                 // JWT 기반 인증 → 세션 사용 안 함
                 .sessionManagement(session ->
@@ -34,6 +39,9 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
+
+                        // OPTIONS 요청 허용
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                         // 루트 및 기본 리소스 허용
                         .requestMatchers(
@@ -75,5 +83,32 @@ public class SecurityConfig {
                 )
 
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+
+            configuration.setAllowedOrigins(List.of(
+                    "http://localhost:5173"
+            ));
+
+            configuration.setAllowedMethods(List.of(
+                    "GET",
+                    "POST",
+                    "PATCH",
+                    "DELETE",
+                    "OPTIONS"
+            ));
+
+            configuration.setAllowedHeaders(List.of("*"));
+
+            configuration.setExposedHeaders(List.of("Authorization"));
+
+            configuration.setAllowCredentials(true);
+
+            return configuration;
+        };
     }
 }
